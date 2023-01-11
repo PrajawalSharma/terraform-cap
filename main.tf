8b3ea280c7bc0a4d14e8a9493692bcbe13e3601b
PrajawalSharma <84971107+PrajawalSharma@users.noreply.github.com>
2023-01-11 15:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,3 +61,44 @@ resource "azurerm_linux_virtual_machine" "example" {
     version   = "20.04.202209200"
   }
 }
+
+resource "null_resource" "null_copy_ssh" {
+    
+    depends_on = [
+    azurerm_linux_virtual_machine.example
+  ]
+    
+  connection {
+    type = "ssh"
+    host = azurerm_linux_virtual_machine.example.public_ip_address
+    user = "adminuser"
+    private_key = file("/var/lib/jenkins/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64",
+        "chmod +x ./kind",
+        "sudo mv ./kind /usr/local/bin/kind",
+        "sudo apt-get update",
+        "sudo apt-get install -y ca-certificates curl gnupg lsb-release",
+        "sudo mkdir -p  /etc/apt/keyrings",
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+        " echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+        "sudo apt-get update",
+        "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+        "sudo kind create cluster",
+        "sudo apt-get install -y ca-certificates curl",
+        "sudo apt-get install -y apt-transport-https",
+        "sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg",
+        "echo \"deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main\" | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+        "sudo apt-get update",
+        "sudo apt-get install -y kubectl",
+        "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3",
+        "chmod 700 get_helm.sh",
+        "./get_helm.sh",
+        "sudo kubectl get nodes"
+    ]
+  }
+}
+
